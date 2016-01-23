19a9173bac6c908682c9b777a969e57639a58d8f
Marc Mauger <marc.mauger@igt.com>
2016-01-23 14:01:31 -0800

    Attempting tagged union for multiple Cell types...failing

---
+++ PolarCell.elm
@@ -0,0 +1,19 @@
+module PolarCell where
+
+import Cell exposing (Cell, CellID)
+
+import Set exposing (..)
+
+-- MODEL
+
+-- Standard cell
+type alias PolarCell = {
+    id: CellID,
+    row: Int,
+    col: Int,
+    masked: Bool,
+    links: Set CellID,
+    outward: Set CellID,
+    parent: Cell
+}
+
