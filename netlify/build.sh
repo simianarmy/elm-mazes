208df7f4bb872c44face83334d4b27b66e159628
MarcMaugerZwift <marc-mauger@zwift.com>
2022-12-02 19:00:58 -0800

    build updates

--- netlify/build.sh
+++ netlify/build.sh
@@ -1,4 +1,4 @@
 curl -L -o elm.tar.gz https://github.com/lydell/elm-old-binaries/releases/download/main/0.18.0-linux-x64.tar.gz
 tar -zxvf elm.tar.gz
 chmod +x dist_binaries/*
-
+dist_binaries/elm-make --yes --output dist/elm.js src/Main.elm
